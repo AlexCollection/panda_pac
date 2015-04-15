a9c9c50150f55a8e061e44243f019ba823c708c6
Maple <maplevalley8@gmail.com>
2015-04-15 17:14:57 +0800

    Optimize pac

--- build.py
+++ build.py
@@ -10,10 +10,12 @@ def read_domains():
         domains = f.read().splitlines()
     with open('res/custom.txt') as f:
         custom_domains = f.read().splitlines()
-    return custom_domains + domains
+    with open('res/proxy.txt') as f:
+        black_domains = f.read().splitlines()
+    return custom_domains + domains, black_domains
 
 
-def generate_pac(domains):
+def generate_pac(domains, black_domains):
     with open('res/proxy.pac') as f:
         proxy_content = f.read()
 
@@ -27,9 +29,17 @@ def generate_pac(domains):
         json.dumps(domains_dict, indent=2, sort_keys=True)
     )
 
+    black_domains_dict = {}
+    for domain in black_domains:
+        black_domains_dict[domain] = 1
+    proxy_content = proxy_content.replace(
+        '__BLACKLIST__',
+        json.dumps(black_domains_dict, indent=2, sort_keys=True)
+    )
+
     return proxy_content
 
 
 if __name__ == '__main__':
-    domains = read_domains()
-    print generate_pac(domains)
+    domains, black_domains = read_domains()
+    print generate_pac(domains, black_domains)
--- res/proxy.pac
+++ res/proxy.pac
@@ -2,6 +2,7 @@ var PROXY = "__PROXY__";
 var DEFAULT = "DIRECT";
 
 var whitelist = __WHITELIST__;
+var blacklist = __BLACKLIST__;
 
 var hasOwnProperty = Object.hasOwnProperty;
 function suffix2(s1,s2){return s1.indexOf(s2,s1.length-s2.length)!==-1};
@@ -11,6 +12,19 @@ function FindProxyForURL(url, host) {
     if (suffix2(host, ".cn")||isPlainHostName(host)||dnsDomainIs(host,".local")||/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/i.test(host)){ return DEFAULT; }
 
     var suffix;
+
+    var pos = host.lastIndexOf('.');
+    for (;;) {
+        suffix = host.substring(pos + 1);
+        if (hasOwnProperty.call(blacklist, suffix)) {
+            return PROXY;
+        }
+        if (pos <= 0) {
+            break;
+        }
+        pos = host.lastIndexOf('.', pos - 1);
+    }
+
     var pos = host.lastIndexOf('.');
     for (;;) {
         suffix = host.substring(pos + 1);
